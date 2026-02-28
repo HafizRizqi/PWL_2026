2ad462913902bcea9d61881424af927b2ddc04d7
HAFIZ RIZQI HERNANDA <hafizhernanda5@gmail.com>
2026-02-28 23:33:16 +0700

    Percobaan Pada Praktikum ke-1 pada hal 1 - 13

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,35 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ('Selamat Datang');
 });
+
+Route::get('/hello', function () {
+ return 'Hello World';
+});
+
+Route::get('/world', function () {
+ return 'World';
+});
+
+Route::get('/about', function () {
+    return ('NIM saya 244107020154, Nama saya Hafiz Rizqi Hernanda');
+});
+
+Route::get('/user/{name}', function ($name) {
+return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function
+($postId, $commentId) {
+ return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{articleId}', function ($articleId) {
+ return 'Halaman artikel dengan ID ke-'.$articleId;
+});
+
+
+
+
+
