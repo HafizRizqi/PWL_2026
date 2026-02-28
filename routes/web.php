1a9c015f7118ead8c642c8b6e8d43b0f8c4d682e
HAFIZ RIZQI HERNANDA <hafizhernanda5@gmail.com>
2026-03-01 05:02:20 +0700

    Percobaan Praktikum ke-2 Controller pada no.7 - 11

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,33 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
 
-Route::get('/', [PageController::class,'index']);
+Route::resource('photos', PhotoController::class);
 
-Route::get('/about', [PageController::class,'about']);
+Route::resource('photos', PhotoController::class)->only([
+ 'index', 'show'
+]);
+Route::resource('photos', PhotoController::class)->except([
+ 'create', 'store', 'update', 'destroy'
+]);
 
-Route::get('/articles/{Id}', [PageController::class,'articles']);
+
+Route::get('/', [HomeController::class,'index']);
+
+Route::get('/about', [AboutController::class,'about']);
+
+Route::get('/articles/{Id}', [ArticleController::class,'articles']);
 
 Route::get('/hello', [WelcomeController::class,'hello']);
 
 Route::get('/world', function () {
  return 'World';
 });
-
-
 
 Route::get('/posts/{post}/comments/{comment}', function
 ($postId, $commentId) {
